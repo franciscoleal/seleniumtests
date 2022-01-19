5f6a3b105a31eabb136158182dbe886f378d79a2
franciscoleal <francisco.lleal@gmail.com>
2022-01-19 10:09:33 -0300

    Selenium Tests

--- Testes/src/br/teste/veiculo/Carro.java
+++
@@ -1,9 +0,0 @@
-package br.teste.veiculo;
-
-class Carro extends Veiculo {
-	
-    public void liga() {
-        System.out.println("Ligando Carro");
-    }
-    
-}
--- Testes/src/br/teste/veiculo/Moto.java
+++
@@ -1,9 +0,0 @@
-package br.teste.veiculo;
-
-class Moto extends Veiculo {
-	
-    public void liga() {
-        System.out.println("Ligando Moto");
-    }
-    
-}
